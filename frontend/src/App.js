ef2904b008b7dcbadcd186febb28d7bbd2b8bdfd
abel2-code <available_email_address@yahoo.com>
2020-08-20 14:39:46 -0500

    did some things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
 import Nav from './Nav';
+import LoginForm from './LoginForm';
+
 
 function App()  {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleClick = () => {
+    setLoggedIn(!loggedIn)
+  }
+
   return (
-    <div className="app">
-      <Nav />
-      <Banner />
-      <Row title="ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchTrending} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    <div>
+      {loggedIn ?
+      <div className="app">
+        <Nav onClick={handleClick} />
+        <Banner />
+        <Row title="ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchTrending} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      </div>
+      :
+      <div onClick={handleClick}>You are not logged in.</div> 
+        }
     </div>
 
   );
